bef91b3e0f469ed8ed54369d7188cb24b37a6b47
Rachel D. Cartwright <stormchica@gmail.com>
2022-08-08 00:55:50 -0500

    add initial setup

---
+++ test/dedupe.test.js
@@ -0,0 +1,11 @@
+const csv = require('./test.csv')
+const chai = require('chai');
+
+const expect = chai.expect;
+
+
+describe('Test Suite', function(){
+  it('should pass', function(){
+    expect(true).to.equal(true);
+  });
+});
