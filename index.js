cb93b847f24e68a344705c25d9c95ab5f72ee2a0
Rachel D. Cartwright <stormchica@gmail.com>
2022-08-09 22:28:08 -0500

    make data iterable

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const fs = require("fs");
 module.exports = {
     readCsv: function () {
         return fs.readFile('./test/test.csv', "utf8", function (err, data) {
-            console.log(data);
+            const [headerRow, ...rows] = data.split('\r\n');
+            const headers = headerRow.split(',');
+
+            return rows.map((row, index) => {
+                const theRow = row.split(',')
+                console.log('theRow', theRow)
+            });
         }); 
     }
 }
